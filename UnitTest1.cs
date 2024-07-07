cfbeab8d3df535f397a0406b67786f4d5ec04c00
Rita-Borges <ryta.borgez@gmail.com>
2024-07-07 18:57:55 +0100

    without refatoring

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,17 +1,35 @@
-/* Demo of https://the-internet.herokuapp.com/ */
-/* Basic element interation */
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumExtras.WaitHelpers;
+
 namespace SeleniumDemoT01;
 
 public class Tests
 {
+    private IWebDriver _driver;
+
     [SetUp]
     public void Setup()
     {
+        _driver = new ChromeDriver();
+        _driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/"); 
+        _driver.Manage().Window.Maximize();
     }
 
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        var linkElement = _driver.FindElement(By.XPath("//ul/li/a[text()='A/B Testing']"));      
+        var actualTitle = linkElement.Text;
+        var expectedTitle = "A/B Testing";
+         
+        Assert.That(actualTitle, Is.EqualTo(expectedTitle));
+    }
+
+    [TearDown]
+    public void CleanUp()
+    {
+        _driver.Quit();   
     }
 }
