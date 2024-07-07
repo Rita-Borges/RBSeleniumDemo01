0feba4d19f57f593ee6aea8fe321b81e624b2a01
Rita-Borges <ryta.borgez@gmail.com>
2024-07-07 16:51:58 +0100

    selenium IDE Included

---
+++ UnitTest1.cs
@@ -0,0 +1,17 @@
+/* Demo of https://the-internet.herokuapp.com/ */
+/* Basic element interation */
+namespace SeleniumDemoT01;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
